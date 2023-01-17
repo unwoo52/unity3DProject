ca8dde46c1608ec01d887372aaf203d4dc44b191
Unwoo <73688472+unwoo52@users.noreply.github.com>
2023-01-17 16:07:40 +0900

    Object Item 코드 취약점 보완 및 Grount Item 프레펩 위치 변경
    ground item 프레펩 위치를 resource\item에서 scriptableObject\...\Item으로 변경

--- Assets/Scripts/ItemScripts/ObjectItem.cs
+++ Assets/Scripts/ItemScripts/ObjectItem.cs
@@ -7,15 +7,45 @@ public class ObjectItem : MonoBehaviour
     public ItemObject item;
     public GameObject groundItem;
     public LayerMask PlayerMask;
+
+    private void Start()
+    {
+        //check grounditem
+        if (this.groundItem == null) 
+        {
+            groundItem = GameObject.Find("Grounditem");
+            if (groundItem != null) return;
+
+            //if failed...
+            Debug.LogError("아이템 드롭 실패"); 
+            Destroy(gameObject); 
+        }
+    }
+
+    private void DropItem(Collision collision)
+    {
+        
+
+        GameObject obj;
+
+
+        obj = Instantiate(this.groundItem, transform.position, new Quaternion(0, 0, 0, 0));
+        obj.layer = this.groundItem.layer;
+        if (obj.TryGetComponent(out GroundItem groundItemScript))
+            groundItemScript.item = item;
+        else
+        {
+            Debug.LogError("치명적 오류. GroundItem에 grountItemScript가 없습니다. 이 오류가 다른 모든 경우에서 발생할 수 있습니다.");
+            Destroy(obj);
+            return;
+        }
+    }
     
     private void OnCollisionEnter(Collision collision)
     {
         if (((1 << collision.gameObject.layer) & PlayerMask) != 0)
         {
-            GameObject obj = Instantiate(this.groundItem, transform.position, new Quaternion(0,0,0,0));
-            obj.layer = this.groundItem.layer;
-            if (obj.TryGetComponent(out GroundItem groundItem))
-                groundItem.item = item;
+            DropItem(collision);
             Destroy(gameObject);
         }
     }
