51b9466f91a611884b58d5b21cd5bf3eb73f1463
Unwoo <73688472+unwoo52@users.noreply.github.com>
2023-02-04 02:35:40 +0900

    study scene commit

---
+++ Assets/Scripts/Study/SSceneLoaderScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SSceneLoaderScript : MonoBehaviour
+{
+    public static SSceneLoaderScript instance = null;
+    public Slider Sliderslider;
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+    public void SceneChange(int i)
+    {
+        StartCoroutine(Loading(i));
+    }
+
+    IEnumerator Loading(int i)
+    {
+        yield return UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("SLoad");
+
+        Sliderslider.gameObject.SetActive(true);
+        Sliderslider.value= 0;
+
+        StartCoroutine(LoadingTarget(i));
+    }
+
+    IEnumerator LoadingTarget(int i)
+    {
+        AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(i);
+        asyncOperation.allowSceneActivation= false;
+        while (!asyncOperation.isDone)
+        {
+            Sliderslider.value = asyncOperation.progress / 0.9f;
+            if(asyncOperation.progress >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        Sliderslider.gameObject.SetActive(false);
+       
+    }
+}
---
+++ Assets/Scripts/Study/STitle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class STitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Application.LoadLevel(5); << 여러 문제를 내포하고 있기 때문에 보통 사용되지 않음
+            //UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(5);
+
+            SSceneLoaderScript.instance.SceneChange(5);
+        }
+    }
+}
